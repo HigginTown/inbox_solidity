999a57b0be57b97d938009e6b748d277d7c0c0d0
Adam Massachi <Adam.Massachi@ibm.com@Adams-MacBook-Pro.local>
2018-01-17 13:19:08 -0600

    tests1

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,4 +1,23 @@
 const assert = require('assert');
-
 const ganache = require('ganache-cli');
+const Web3 = require('web3');
+
+const web3 = new Web3(ganache.provider());
+
+
+beforeEach(() => {
+	// get a list of all accounts
+
+	web3.eth.getAccounts().then(fetchedAccounts => {
+			console.log(fetchedAccounts);
+		});
+
+	// use an account to deploy a contract
+});
+
+describe("inbox", () => {
+	it('deploys a contract', () => {
+
+	});
+});
 
