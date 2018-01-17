2373c73961bef20bf1b8e133532a06f9f2b51e18
Adam Massachi <Adam.Massachi@ibm.com@Adams-MacBook-Pro.local>
2018-01-17 13:52:02 -0600

    first deployment

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,23 +1,31 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-
+const {interface, bytecode} = require("../compile");
 const web3 = new Web3(ganache.provider());
 
+let accounts; 
+let inbox;
 
-beforeEach(() => {
+
+
+beforeEach(async () => {
 	// get a list of all accounts
 
-	web3.eth.getAccounts().then(fetchedAccounts => {
-			console.log(fetchedAccounts);
-		});
+	accounts = await web3.eth.getAccounts();
 
 	// use an account to deploy a contract
+
+	inbox = await new web3.eth.Contract(JSON.parse(interface))
+		.deploy({ data: bytecode, arguments: ["Hithereptilebud"]})
+		.send({ from: accounts[0], gas: '1000000' });
+
 });
 
 describe("inbox", () => {
 	it('deploys a contract', () => {
-
+		console.log(inbox); 
 	});
 });
+
 
